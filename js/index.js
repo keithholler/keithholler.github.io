54a45e52ab62463aab6199bdcb1e2aaf2bf6e24e
Keith Holler <robert.keith.holler@gmail.com>
2020-08-14 12:25:15 -0400

    particle view

--- js/index.js
+++ js/index.js
@@ -6,25 +6,9 @@ function line(particle, particle2) {
   }
    
   function animate() {
-    let canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');
-  
-  // Set display size (css pixels).
-  var size = 400;
-  canvas.style.width = size + "px";
-  canvas.style.height = size + "px";
-  
-  // Set actual size in memory (scaled to account for extra pixel density).
-  var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
-  canvas.width = Math.floor(size * scale);
-  canvas.height = Math.floor(size * scale);
-  // Normalize coordinate system to use css pixels.
-context.scale(scale, scale);
-context.fillStyle = 'white';
     context.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < maxParticles; i++) {
       let particle = particles[i];
-     
       context.fillRect(particle.x - particleSize / 2, particle.y - particleSize / 2, particleSize, particleSize);
       for (let j = 0; j < maxParticles; j++) {
         if (i != j) {
@@ -33,14 +17,14 @@ context.fillStyle = 'white';
           let distanceY = Math.abs(particle.y - particle2.y);
           if (distanceX < threshold && distanceY < threshold) {
             context.lineWidth = ((threshold * 2) - (distanceX + distanceY)) / 50;
-            
+            let color = 200 - Math.floor(distanceX + distanceY);
             context.strokeStyle = '#FFFFFF';
             line(particle, particle2);
           }
         }
       }
       particle.x = particle.x + -particle.vx;
-      particle.y = particle.y + particle.vy;
+      particle.y = particle.y + -particle.vy;
       if (particle.x > canvas.width - particleSize || particle.x < particleSize)
         particle.vx = -particle.vx;
       if (particle.y > canvas.height - particleSize || particle.y < particleSize)
@@ -62,13 +46,12 @@ var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blu
 canvas.width = Math.floor(size * scale);
 canvas.height = Math.floor(size * scale);
 
-// Normalize coordinate system to use css pixels.
-context.scale(scale, scale);
+
 
   let particles = [];
-  let particleSize = 4;
-  let maxParticles = 25;
-  let threshold = 50;
+  let particleSize = 10;
+  let maxParticles = 20;
+  let threshold = 120;
   for (let i = 0; i < maxParticles; i++) {
     let particle = {
       x: Math.random() * canvas.width,
